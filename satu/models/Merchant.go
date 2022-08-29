a236ee80b5d46be661f85d326067186d6b01dc47
zSANSANz <sandipermana88@gmail.com>
2022-08-29 17:49:49 +0700

    feat: adding realationship beetween user and merchant

--- satu/models/Merchant.go
+++ satu/models/Merchant.go
@@ -7,6 +7,7 @@ import (
 type Merchant struct {
 	Id           uint      `gorm:"type:bigint(20);not null" json:"id" form:"id"`
 	UserId       uint      `gorm:"type:int(40);not null" json:"user_id" form:"user_id"`
+	User         User      `gorm:"foreignKey:UserId" json:"user" form:"user"`
 	MerchantName string    `gorm:"type:varchar(40);not null" json:"merchant_name" form:"merchant_name"`
 	CreatedAt    time.Time `gorm:"type:bigint(20)" json:"created_at" form:"created_at"`
 	CreatedBy    uint      `json:"created_by" form:"created_by"`
